ceb83fa2d52e7a06bf67220a382413882d7da1d2
adam <adamliliemark@gmail.com>
2022-12-02 09:11:02 +0100

    2022-02 naïve but simple

---
+++ 2022/02/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/adamliliemark/advent-of-code/utils"
+)
+
+func battle(opponentChoice, ownChoice string) string {
+	switch opponentChoice {
+	case "A":
+		if ownChoice == "Y" {
+			return "win"
+		} else if ownChoice == "X" {
+			return "draw"
+		}
+	case "B":
+		if ownChoice == "Z" {
+			return "win"
+		} else if ownChoice == "Y" {
+			return "draw"
+		}
+	case "C":
+		if ownChoice == "X" {
+			return "win"
+		} else if ownChoice == "Z" {
+			return "draw"
+		}
+	}
+	return "lose"
+}
+
+func part1(input []string) int {
+	var score int
+	for _, line := range input {
+		v := strings.Fields(line)
+		switch v[1] {
+		case "X":
+			score += 1
+		case "Y":
+			score += 2
+		case "Z":
+			score += 3
+		}
+
+		switch battle(v[0], v[1]) {
+		case "win":
+			score += 6
+		case "draw":
+			score += 3
+		case "lose":
+			score += 0
+		}
+	}
+	return score
+}
+
+func part2(input []string) int {
+	var score int
+	for _, line := range input {
+		v := strings.Fields(line)
+		switch v[1] {
+		case "X":
+			switch v[0] {
+			case "A":
+				score += 3
+			case "B":
+				score += 1
+			case "C":
+				score += 2
+			}
+		case "Y":
+			score += 3
+			switch v[0] {
+			case "A":
+				score += 1
+			case "B":
+				score += 2
+			case "C":
+				score += 3
+			}
+		case "Z":
+			score += 6
+			switch v[0] {
+			case "A":
+				score += 2
+			case "B":
+				score += 3
+			case "C":
+				score += 1
+			}
+		}
+	}
+
+	return score
+}
+
+func main() {
+	input := utils.ReadFileLineByLine("input.txt")
+
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
