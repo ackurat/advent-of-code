c407ea385199dbdc51a2573c42b7e21d5af5aa80
Adam Liliemark <adamliliemark@gmail.com>
2024-12-01 17:04:06 +0100

    reorg

--- 2015/go/02/main.go
+++ 2015/go/02/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/adamliliemark/advent-of-code/utils"
+	"github.com/ackurat/advent-of-code/utils"
 )
 
 type Box struct {
--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/adamliliemark/advent-of-code
-
-go 1.19
